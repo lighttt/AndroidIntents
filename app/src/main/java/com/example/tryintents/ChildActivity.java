c70bef30ed51566199aad15bda65e4514334819e
Manish Tuladhar <manish.eclipse@gmail.com>
2020-06-03 11:20:00 +0545

    Completed Explicit Intents

---
+++ app/src/main/java/com/example/tryintents/ChildActivity.java
@@ -0,0 +1,24 @@
+package com.example.tryintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ChildActivity extends AppCompatActivity {
+
+    private TextView mDisplayText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+        mDisplayText = findViewById(R.id.tv_display);
+        Intent intentThatStartedThisActivity = getIntent();
+        if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
+            String textEntered = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+            mDisplayText.setText(textEntered);
+        }
+    }
+}
--- app/src/main/java/com/example/tryintents/MainActivity.java
+++ app/src/main/java/com/example/tryintents/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.tryintents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mNameEntry;
+    private Button mDoSomethingCoolButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDoSomethingCoolButton = findViewById(R.id.b_do_something_cool);
+        mNameEntry =  findViewById(R.id.et_text_entry);
+
+        mDoSomethingCoolButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String textEntered = mNameEntry.getText().toString();
+                Context context = MainActivity.this;
+                Class destinationActivity = ChildActivity.class;
+                Intent startChildActivityIntent = new Intent(context, destinationActivity);
+                startChildActivityIntent.putExtra(Intent.EXTRA_TEXT, textEntered);
+                startActivity(startChildActivityIntent);
+            }
+        });
     }
 }
